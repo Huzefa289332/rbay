8c9e966557c669ce8e12709a5ed557844f24c24c
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2024-05-08 17:12:57 +0500

    locking

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,4 +1,40 @@
-export const withLock = async () => {};
+import { randomBytes } from 'crypto';
+import { client } from './client';
+
+export const withLock = async (key: string, cb: () => any) => {
+	// Initialize a few variables to control retry behavior
+	const retryDelayMs = 100;
+	let retries = 20;
+
+	// Generate a random value to store at the lock key
+	const token = randomBytes(6).toString('hex');
+
+	// Create the lock key
+	const lockKey = `lock:${key}`;
+
+	// Set up a while loop to implement the retry behavior
+	while (retries >= 0) {
+		retries--;
+
+		// Try to do a SET NX operation
+		const acquired = await client.set(lockKey, token, { NX: true, PX: 2000 });
+
+		if (!acquired) {
+			// Brief pause (retryDelayMs) and then retry
+			await pause(retryDelayMs);
+			continue;
+		}
+
+		// IF the set is successful then run the callback
+		try {
+			const result = await cb();
+			return result;
+		} finally {
+			// Unset the lock key
+			await client.del(lockKey);
+		}
+	}
+};
 
 const buildClientProxy = () => {};
 
